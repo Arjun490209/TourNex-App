00501564182683515d7294af5a3e441c39998df2
Arjun Prajapati <colorfulz099@gmail.com>
2026-04-17 21:21:15 +0530

    Change server listen address to '0.0.0.0'

--- backend/index.js
+++ backend/index.js
@@ -31,6 +31,8 @@ app.use("/api", tournamentRoutes);
 // port
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
